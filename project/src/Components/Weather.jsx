a20df10f0fb070d1f1408061ea38dad01a5e9556
PriyankaGurav0105 <priyankagurav0105@gmail.com>
2024-12-22 20:39:27 +0530

    Created the basic file structure

---
+++ project/src/Components/Weather.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './Weather.css'
+
+const Weather = () => {
+  return (
+    <div>
+      <h1>Weather App</h1>
+    </div>
+  )
+}
+
+export default Weather
